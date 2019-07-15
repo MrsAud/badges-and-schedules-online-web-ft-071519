6a3deaa6d87f69e2ee79bfb2ed0b38a13a7d758c
Audrea Cook <audreacwork@gmail.com>
2019-07-15 19:05:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,7 +26,7 @@ end
 
 def printer(attendees)
   attendees.each do |name|
-    batch_badge_creator(name)
-    assign_rooms(name)
+    puts batch_badge_creator(name)
+    puts assign_rooms(name)
   end
 end
